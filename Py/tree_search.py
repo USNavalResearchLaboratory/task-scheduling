3ee0103aff8f0a3a00b5426da40b5c4214f23d8f
rademacherp <paul.rademacher@nrl.navy.mil>
2020-05-06 16:40:50 -0400

    Modified B&B with initial and conditional roll-outs in support of time-restricted operation.
    Signed-off-by: rademacherp <paul.rademacher@nrl.navy.mil>

--- Py/tree_search.py
+++ Py/tree_search.py
@@ -295,7 +295,7 @@ def _check_loss(tasks: list, node: TreeNode):
 
 def branch_bound(tasks: list, ch_avail: list, exhaustive=False, verbose=False, rng=rng_default):
     """Branch and Bound algorithm.
-    
+
     Parameters
     ----------
     tasks : list of TaskRRM
@@ -317,45 +317,39 @@ def branch_bound(tasks: list, ch_avail: list, exhaustive=False, verbose=False, r
 
     """
 
-    TreeNode._tasks = tasks         # TODO: proper style to redefine class attribute here?
+    TreeNode._tasks = tasks  # TODO: proper style to redefine class attribute here?
     TreeNode._ch_avail_init = ch_avail
     TreeNode._exhaustive = exhaustive
     TreeNode._rng = rng
 
     n_ch = len(ch_avail)
 
-    stack = [TreeNodeBound([[] for _ in range(n_ch)])]      # Initialize Stack
-    l_upper_min = stack[0].l_up
+    stack = [TreeNodeBound([[] for _ in range(n_ch)])]  # Initialize Stack
+
+    node_best = stack[0].roll_out(do_copy=True)     # roll-out initial solution
+    l_best = node_best.l_ex
 
     # Iterate
-    while not ((len(stack) == 1) and (len(stack[0].seq_rem) == 0)):
+    while len(stack) > 0:
         if verbose:
-            print(f'# Remaining Nodes = {len(stack)}, Loss < {l_upper_min:.3f}', end='\r')
+            print(f'# Remaining Nodes = {len(stack)}, Loss < {l_best:.3f}', end='\r')
 
-        node = stack.pop()     # Extract Node
+        node = stack.pop()  # Extract Node
 
         # Branch
         for node_new in node.branch(do_permute=True):
             # Bound
-            if node_new.l_lo < l_upper_min:  # New node is not dominated
-                if node_new.l_up < l_upper_min:
-                    l_upper_min = node_new.l_up
-                    stack = [s for s in stack if s.l_lo < l_upper_min]  # Cut Dominated Nodes
+            if node_new.l_lo < l_best:  # New node is not dominated
+                if node_new.l_up < l_best:
+                    node_best = node_new.roll_out(do_copy=True)     # roll-out a new best node
+                    l_best = node_best.l_ex
+                    stack = [s for s in stack if s.l_lo < l_best]  # Cut Dominated Nodes
 
-                if len(node_new.seq_rem) > 0:  # Add New Node to Stack
-                    stack.append(node_new)     # LIFO
-                else:
-                    stack.insert(0, node_new)
+                stack.append(node_new)  # Add New Node to Stack, LIFO
 
-    if len(stack) != 1:
-        raise ValueError('Multiple nodes...')
+    _check_loss(tasks, node_best)
 
-    if not all([s.l_lo == s.l_up for s in stack]):
-        raise ValueError('Node bounds do not converge.')
-
-    _check_loss(tasks, stack[0])
-
-    t_ex, ch_ex = stack[0].t_ex, stack[0].ch_ex      # optimal
+    t_ex, ch_ex = node_best.t_ex, node_best.ch_ex  # optimal
 
     return t_ex, ch_ex
 
