efef72259855df40cebf3dd22941ee92235b9d51
Kevin Wagner <kevin.wagner@nrl.navy.mil>
2020-08-21 14:25:59 -0400

    Building towards priority list RL algorithm

--- Py/RL_Priority/RL_priority_train.py
+++ Py/RL_Priority/RL_priority_train.py
@@ -12,6 +12,53 @@ from RL_Priority.env_priority import PriorityQueue
 
 Queue = PriorityQueue()
 
+NumSteps = Queue.NumSteps
+NumEpoch = 10
+reward = np.empty([NumEpoch, NumSteps])
+for i_episode in range(NumEpoch):
+    observation = Queue.reset()
+    timeSec = Queue.timeSec
+    RP = Queue.env_config.get("RP")
+
+    for ii in np.arange(0, NumSteps):  # Main Loop to evaluate schedulers (Start at 1 because PriorityQueue() Initializes time 0
+        timeSec = ii * RP  # Current time
+
+        ChannelAvailableTime = Queue.env_config.get("ChannelAvailableTime")
+        Queue.timeSec = timeSec
+
+        if np.min(ChannelAvailableTime) > timeSec:
+            # RunTime[ii, idx_alg] = math.nan
+            # Cost[ii, idx_alg] = math.nan
+            continue  # Jump to next Resource Period
+
+
+
+        action = Queue.action_space.sample()  # Insert your policy here
+
+        N = Queue.env_config.get("N")
+        action = np.ones(N, dtype='int64')  # Always choose first action available
+
+        ob_next, reward[i_episode, ii], done, info = Queue.step(action)
+
+    # for t in range(100):
+    #     # env.render()
+    #     print(observation)
+    #     action = env.action_space.sample()
+    #     observation, reward, done, info = env.step(action)
+    #     if done:
+    #         print("Episode finished after {} timesteps".format(t+1))
+    #         break
+    # env.close()
+
+
+
+
+
+
+# Plot Rewards
+plt.figure(1)
+plt.plot(np.mean(reward, axis=0))
+
 # Reassess Track Priorities
 job = Queue.job
 timeSec = Queue.timeSec
