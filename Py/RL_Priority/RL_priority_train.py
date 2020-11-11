b297cd6ed69b336756b239f2985866e10276d180
Kevin Wagner <kevin.wagner@nrl.navy.mil>
2020-11-11 15:47:48 -0500

    My version of RL scheduling. Appears to be learning something. Plan to simplify the problem further to better understand.
    Current N = 3
    Action Space = 3x2x1
    Observation Space = 10*10*10 x 2*2*2 x 5*5*5 x 5*5*5

--- Py/RL_Priority/RL_priority_train.py
+++ Py/RL_Priority/RL_priority_train.py
@@ -9,12 +9,76 @@ import matplotlib.pyplot as plt
 # sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../anotherproject")
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-from RL_Priority.env_priority import PriorityQueue, PriorityQueueDiscrete
+from RL_Priority.env_priority import PriorityQueue, PriorityQueueDiscrete, TaskAssignmentDiscrete
 from RL_Priority.env_priority import DiscretizedObservationWrapper
 from collections import defaultdict
-from stable_baselines import A2C
+from stable_baselines import A2C, PPO2
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.env_checker import check_env
+
+from stable_baselines.common.evaluation import evaluate_policy
+from stable_baselines.bench.monitor import Monitor
+from stable_baselines.results_plotter import load_results, ts2xy
+
+
+
+best_mean_reward, n_steps = -np.inf, 0
+
+def callback(_locals, _globals):
+  """
+  Callback called at each step (for DQN an others) or after n steps (see ACER or PPO2)
+  :param _locals: (dict)
+  :param _globals: (dict)
+  """
+  global n_steps, best_mean_reward
+  # Print stats every 1000 calls
+  if (n_steps + 1) % 100 == 0:
+      # Evaluate policy performance
+      x, y = ts2xy(load_results(log_dir), 'timesteps')
+      if len(x) > 0:
+          mean_reward = np.mean(y[-100:])
+          print(x[-1], 'timesteps')
+          print("Best mean reward: {:.2f} - Last mean reward per episode: {:.2f}".format(best_mean_reward, mean_reward))
+
+          # New best model, you could save the agent here
+          if mean_reward > best_mean_reward:
+              best_mean_reward = mean_reward
+              # Example for saving best model
+              print("Saving new best model")
+              _locals['self'].save(log_dir + 'best_model.pkl')
+  n_steps += 1
+  return False
+
+
+def moving_average(values, window):
+    """
+    Smooth values by doing a moving average
+    :param values: (numpy array)
+    :param window: (int)
+    :return: (numpy array)
+    """
+    weights = np.repeat(1.0, window) / window
+    return np.convolve(values, weights, 'valid')
+
+def plot_results(log_folder, title='Learning Curve', figNum = 1):
+    """
+    plot the results
+
+    :param log_folder: (str) the save location of the results to plot
+    :param title: (str) the title of the task to plot
+    """
+    x, y = ts2xy(load_results(log_folder), 'timesteps')
+    y = moving_average(y, window=1000)
+    # Truncate x
+    x = x[len(x) - len(y):]
+
+    fig = plt.figure(figNum)
+    # fig = plt.figure(title=title)
+    plt.plot(x, y)
+    plt.xlabel('Number of Timesteps')
+    plt.ylabel('Rewards')
+    plt.title(title + " Smoothed")
+    plt.show()
 
 
 def evaluate(model, num_steps=1000):
@@ -44,18 +108,62 @@ def evaluate(model, num_steps=1000):
     return mean_100ep_reward
 
 
-env = PriorityQueueDiscrete()
-check_env(env)
+# env = PriorityQueueDiscrete()
 
 
-model = A2C(MlpPolicy, env, verbose=1)
-mean_reward_before_train = evaluate(model, num_steps=10000)
+# Create log dir
+log_dir = "./tmp/gym/"
+os.makedirs(log_dir, exist_ok=True)
+# env = gym.make('LunarLanderContinuous-v2')
 
-model.learn(total_timesteps=25000)
+env = TaskAssignmentDiscrete()
+env = Monitor(env, log_dir, allow_early_resets=True)
+# env.reset()
+# action = env.action_space.sample()
+# env.step(action)
 
-mean_reward = evaluate(model, num_steps=10000)
+
+# check_env(env)
+
+
+model = A2C(MlpPolicy, env, verbose=1, learning_rate=0.005)
+model_PPO2 = PPO2(MlpPolicy, env, verbose=1, learning_rate=0.005)
+# model = A2C(MlpPolicy, env, verbose=1, tensorboard_log="./log_tensorboard/")
+
+
+
+
+model.learn(total_timesteps=1000)
+plot_results(log_dir, figNum=1)
+
+model_PPO2.learn(total_timesteps=10000)
+plot_results(log_dir, figNum=2)
+
+# model.learn(total_timesteps=200, tb_log_name="first_run")
+# model.learn(total_timesteps=200, callback=callback)
+
+# std_reward = np.std(episode_rewards)
+# mean_reward_before_train = evaluate(model, num_steps=1000)
+
+
+episode_rewards, episode_lengths = evaluate_policy(model, env, n_eval_episodes=100, return_episode_rewards=True)
+episode_rewards_train, episode_lengths_train = evaluate_policy(model, env, n_eval_episodes=100, return_episode_rewards=True)
+
+
+mean_reward_rolling = moving_average(episode_rewards, 10)
+mean_reward_rolling_train = moving_average(episode_rewards_train, 10)
+
+plt.figure(2)
+plt.clf()
+plt.plot(mean_reward_rolling, label='Before Train')
+plt.plot(mean_reward_rolling_train, label='After Train')
+plt.legend
+plt.show()
+# mean_reward = evaluate(model, num_steps=10000)
+
+
+
 a = 1
-
 
 
 # from stable_baselines.common.policies import MlpPolicy
@@ -76,147 +184,147 @@ a = 1
 #     obs, rewards, dones, info = env.step(action)
 #     env.render()
 
+##
 
-
-Q = defaultdict(float)
-gamma = 0.99  # Discounting factor
-alpha = 0.5  # soft update param
-
-
-# env = PriorityQueue()
-# # Instantiate the agent
-# model = DQN('MlpPolicy', env, learning_rate=1e-3, prioritized_replay=True, verbose=1)
-# # Train the agent
-# model.learn(total_timesteps=int(2e5))
+# Q = defaultdict(float)
+# gamma = 0.99  # Discounting factor
+# alpha = 0.5  # soft update param
 #
 #
-# env = gym.make("CartPole-v0")
+# # env = PriorityQueue()
+# # # Instantiate the agent
+# # model = DQN('MlpPolicy', env, learning_rate=1e-3, prioritized_replay=True, verbose=1)
+# # # Train the agent
+# # model.learn(total_timesteps=int(2e5))
+# #
+# #
+# # env = gym.make("CartPole-v0")
+# #
+# # env = DiscretizedObservationWrapper(
+# #     env,
+# #     n_bins=8,
+# #     low=[-2.4, -2.0, -0.42, -3.5],
+# #     high=[2.4, 2.0, 0.42, 3.5]
+# # )
+# #
+# #
+# # actions = range(env.action_space)
+# #
+# # def update_Q(s, r, a, s_next, done):
+# #     max_q_next = max([Q[s_next, a] for a in actions])
+# #     # Do not include the next state's value if currently at the terminal state.
+# #     Q[s, a] += alpha * (r + gamma * max_q_next * (1.0 - done) - Q[s, a])
 #
-# env = DiscretizedObservationWrapper(
-#     env,
-#     n_bins=8,
-#     low=[-2.4, -2.0, -0.42, -3.5],
-#     high=[2.4, 2.0, 0.42, 3.5]
-# )
+#
+# def main():
+#
+#     policy = 'priority' # 'first', 'random'
+#
+#     Queue = PriorityQueue()
+#
+#     NumSteps = Queue.NumSteps
+#     NumEpoch = 10
+#     reward = np.empty([NumEpoch, NumSteps])
+#     for i_episode in range(NumEpoch):
+#         observation = Queue.reset()
+#         timeSec = Queue.timeSec
+#         RP = Queue.env_config.get("RP")
+#
+#         for ii in np.arange(0, NumSteps):  # Main Loop to evaluate schedulers (Start at 1 because PriorityQueue() Initializes time 0
+#             timeSec = ii * RP  # Current time
+#
+#             ChannelAvailableTime = Queue.env_config.get("ChannelAvailableTime")
+#             Queue.timeSec = timeSec
+#
+#             if np.min(ChannelAvailableTime) > timeSec:
+#                 # RunTime[ii, idx_alg] = math.nan
+#                 # Cost[ii, idx_alg] = math.nan
+#                 continue  # Jump to next Resource Period
+#
+#             if policy == 'priority':
+#                 # Reassess Track Priorities
+#                 job = Queue.job
+#                 for jj in range(len(job)):
+#                     job[jj].Priority = job[jj](timeSec)
+#                 priority = np.array([task.Priority for task in job])
+#                 priority_Idx = np.argsort(-1*priority, kind='mergesort')
+#                 N = Queue.env_config.get('N')
+#                 action = np.zeros(N,dtype='int64')
+#                 possible_actions = np.arange(0,Queue.M)
+#                 for jj in range(N):
+#                     index = np.where(possible_actions == priority_Idx[jj])
+#                     action[jj] = index[0]
+#                     possible_actions = np.delete(possible_actions, index[0])
+#                     # priority_Idx = np.delete(priority_Idx, 0)
+#                     # priority_Idx = priority_Idx - 1 # Decrement by 1 to capture new indicies
+#             elif policy == 'random':
+#                 action = Queue.action_space.sample()  # Insert your policy here
+#             elif policy == 'first':
+#                 N = Queue.env_config.get("N")
+#                 action = np.zeros(N, dtype='int64')  # Always choose first action available
+#             else:
+#                 print('Policy unrecognized')
+#
+#             ob_next, reward[i_episode, ii], done, info = Queue.step(action)
+#
+#         # for t in range(100):
+#         #     # env.render()
+#         #     print(observation)
+#         #     action = env.action_space.sample()
+#         #     observation, reward, done, info = env.step(action)
+#         #     if done:
+#         #         print("Episode finished after {} timesteps".format(t+1))
+#         #         break
+#         # env.close()
 #
 #
-# actions = range(env.action_space)
 #
-# def update_Q(s, r, a, s_next, done):
-#     max_q_next = max([Q[s_next, a] for a in actions])
-#     # Do not include the next state's value if currently at the terminal state.
-#     Q[s, a] += alpha * (r + gamma * max_q_next * (1.0 - done) - Q[s, a])
-
-
-def main():
-
-    policy = 'priority' # 'first', 'random'
-
-    Queue = PriorityQueue()
-
-    NumSteps = Queue.NumSteps
-    NumEpoch = 10
-    reward = np.empty([NumEpoch, NumSteps])
-    for i_episode in range(NumEpoch):
-        observation = Queue.reset()
-        timeSec = Queue.timeSec
-        RP = Queue.env_config.get("RP")
-
-        for ii in np.arange(0, NumSteps):  # Main Loop to evaluate schedulers (Start at 1 because PriorityQueue() Initializes time 0
-            timeSec = ii * RP  # Current time
-
-            ChannelAvailableTime = Queue.env_config.get("ChannelAvailableTime")
-            Queue.timeSec = timeSec
-
-            if np.min(ChannelAvailableTime) > timeSec:
-                # RunTime[ii, idx_alg] = math.nan
-                # Cost[ii, idx_alg] = math.nan
-                continue  # Jump to next Resource Period
-
-            if policy == 'priority':
-                # Reassess Track Priorities
-                job = Queue.job
-                for jj in range(len(job)):
-                    job[jj].Priority = job[jj](timeSec)
-                priority = np.array([task.Priority for task in job])
-                priority_Idx = np.argsort(-1*priority, kind='mergesort')
-                N = Queue.env_config.get('N')
-                action = np.zeros(N,dtype='int64')
-                possible_actions = np.arange(0,Queue.M)
-                for jj in range(N):
-                    index = np.where(possible_actions == priority_Idx[jj])
-                    action[jj] = index[0]
-                    possible_actions = np.delete(possible_actions, index[0])
-                    # priority_Idx = np.delete(priority_Idx, 0)
-                    # priority_Idx = priority_Idx - 1 # Decrement by 1 to capture new indicies
-            elif policy == 'random':
-                action = Queue.action_space.sample()  # Insert your policy here
-            elif policy == 'first':
-                N = Queue.env_config.get("N")
-                action = np.zeros(N, dtype='int64')  # Always choose first action available
-            else:
-                print('Policy unrecognized')
-
-            ob_next, reward[i_episode, ii], done, info = Queue.step(action)
-
-        # for t in range(100):
-        #     # env.render()
-        #     print(observation)
-        #     action = env.action_space.sample()
-        #     observation, reward, done, info = env.step(action)
-        #     if done:
-        #         print("Episode finished after {} timesteps".format(t+1))
-        #         break
-        # env.close()
-
-
-
-
-
-
-    # Plot Rewards
-    plt.figure(1)
-    mean_reward = np.mean(reward, axis=0)
-    plt.plot(RP*np.arange(len(mean_reward)),  np.mean(reward, axis=0))
-    plt.xlabel('Time (s)')
-    plt.show()
-
-if __name__ == '__main__':
-    main()
-
-if 0:
-    # Reassess Track Priorities
-    job = Queue.job
-    timeSec = Queue.timeSec
-    for jj in range(len(job)):
-        job[jj].Priority = job[jj](timeSec)
-
-    priority = np.array([task.Priority for task in job])
-    priority_Idx = np.argsort(-1*priority, kind='mergesort') # Note: default 'quicksort' gives strange ordering Note: Multiple by -1 to reverse order or [::-1] reverses sort order to be descending.
-
-
-    # from kneed import KneeLocator
-
-
-    # Load Pickled Data
-    print(os.getcwd())
-    filename = './RL_data/RLP_data_2020-08-06_09-53-56'
-    infile = open(filename, 'rb')
-    data_dict = pickle.load(infile)
-    infile.close()
-
-    a = 1
-
-    F = data_dict.get('F')
-    feat_record = data_dict.get('feat_record')
-    feature_names = data_dict.get('feature_names')
-    a = 1
-
-    for jj in range(len(feat_record)):
-        plt.figure(jj)
-        plt.style.use("fivethirtyeight")
-        plt.hist(F[:,jj])
-        plt.title(feature_names[jj])
+#
+#
+#
+#     # Plot Rewards
+#     plt.figure(1)
+#     mean_reward = np.mean(reward, axis=0)
+#     plt.plot(RP*np.arange(len(mean_reward)),  np.mean(reward, axis=0))
+#     plt.xlabel('Time (s)')
+#     plt.show()
+#
+# if __name__ == '__main__':
+#     main()
+#
+# if 0:
+#     # Reassess Track Priorities
+#     job = Queue.job
+#     timeSec = Queue.timeSec
+#     for jj in range(len(job)):
+#         job[jj].Priority = job[jj](timeSec)
+#
+#     priority = np.array([task.Priority for task in job])
+#     priority_Idx = np.argsort(-1*priority, kind='mergesort') # Note: default 'quicksort' gives strange ordering Note: Multiple by -1 to reverse order or [::-1] reverses sort order to be descending.
+#
+#
+#     # from kneed import KneeLocator
+#
+#
+#     # Load Pickled Data
+#     print(os.getcwd())
+#     filename = './RL_data/RLP_data_2020-08-06_09-53-56'
+#     infile = open(filename, 'rb')
+#     data_dict = pickle.load(infile)
+#     infile.close()
+#
+#     a = 1
+#
+#     F = data_dict.get('F')
+#     feat_record = data_dict.get('feat_record')
+#     feature_names = data_dict.get('feature_names')
+#     a = 1
+#
+#     for jj in range(len(feat_record)):
+#         plt.figure(jj)
+#         plt.style.use("fivethirtyeight")
+#         plt.hist(F[:,jj])
+#         plt.title(feature_names[jj])
 
 
 
